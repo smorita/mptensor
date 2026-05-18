461e2075de1a8bd115880d76f6121f5acca85075
Satoshi Morita <smorita@keio.jp>
2026-05-18 15:27:29 +0900

    Modify print statements in output.py for Python 3 compatibility

--- examples/output/output.py
+++ examples/output/output.py
@@ -30,7 +30,7 @@ A2 = np.zeros(shape)
 for i0 in range(shape[0]):
     for i1 in range(shape[1]):
         A2[i0, i1] = i0 * 3 + i1
-print A2
+print(A2)
 
 shape = (2, 3, 4)
 A3 = np.zeros(shape)
@@ -38,7 +38,7 @@ for i0 in range(shape[0]):
     for i1 in range(shape[1]):
         for i2 in range(shape[2]):
             A3[i0, i1, i2] = i0 * 12 + i1 * 4 + i2
-print A3
+print(A3)
 
 shape = (2, 3, 4, 5)
 A4 = np.zeros(shape)
@@ -47,4 +47,4 @@ for i0 in range(shape[0]):
         for i2 in range(shape[2]):
             for i3 in range(shape[3]):
                 A4[i0, i1, i2, i3] = i0 * 60 + i1 * 20 + i2 * 5 + i3
-print A4
+print(A4)
